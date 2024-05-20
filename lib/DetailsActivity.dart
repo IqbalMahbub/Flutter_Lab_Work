ed14e2dc67b1e307617e5d13a4a7c63cb75c74fa
iqbal <iqbalmahbub420@gmail.com>
2024-05-21 04:40:05 +0600

    Details page design

--- lib/DetailsActivity.dart
+++ lib/DetailsActivity.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:lab_work/main.dart';
 
@@ -7,14 +6,51 @@ import 'AppBarLeading.dart';
 
 class DetailsActivity extends StatelessWidget {
   static const details ='It is details page';
+  static const title = 'Details Page';
+
+  const DetailsActivity({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar:AppBar(leading: AppBarLeading(),title: const Text
-          (title),
-          backgroundColor: Colors.pink.shade100,
-        actions: [actionBar()],),
-      body: const Text(details),
-    );
+    return
+        Scaffold(
+          backgroundColor: Colors.black,
+          body: DefaultTabController(
+             length: 4,
+             child: Scaffold(
+                 appBar: AppBar(
+                   backgroundColor: Colors.transparent, 
+                   elevation: 0, 
+                   iconTheme: const IconThemeData(
+                     color: Colors.grey 
+                   ), 
+                   title: const Text(title, style: TextStyle(color: Colors.grey)), 
+                   actions: [actionBar()],
+                   bottom: const TabBar(
+                     indicatorColor: Colors.black54, 
+                     tabs: [
+                       Tab(
+                         icon: Icon(Icons.home,color: Colors.grey,),
+                         child: Text('Overview',style: TextStyle(fontWeight: FontWeight.bold,color: Colors.red),),
+                       ),
+                       Tab(
+                         icon: Icon(Icons.favorite,color: Colors.grey,),
+                         child: Text('Docs',style: TextStyle(fontWeight:
+                         FontWeight.bold,color: Colors.red),),
+                       ),
+                       Tab(
+                         icon: Icon(Icons.list,color: Colors.grey,),
+                         child: Text('Information',style: TextStyle(fontWeight: FontWeight.bold,color: Colors.red),),
+                       ),
+                       Tab(
+                         icon: Icon(Icons.home,color: Colors.grey,),
+                         child: Text('Overview',style: TextStyle(fontWeight: FontWeight.bold,color: Colors.red),),
+                       )
+                   ],
+                   ),
+          
+               )
+            )
+          )
+        );
   }
 }
